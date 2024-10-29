05d6294e880c18ed4c10963c1b3bb455dd838264
inncent <innokentiykim90@gmail.com>
2024-10-29 21:42:46 +0500

    Choices field

--- women/models.py
+++ women/models.py
@@ -3,12 +3,16 @@ from django.urls import reverse
 
 
 class Women(models.Model):
+    class Status(models.IntegerChoices):
+        DRAFT = 0, "Черновик"
+        PUBLISHED = 1, "Опубликовано"
+        
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(choices=Status.choices, default=Status.PUBLISHED)
     
 
     def __str__(self):
